1c510e5930992839b2f3a8ef93cc830754c85885
Eder <ezapataa1@eafit.edu.co>
2022-02-10 11:32:01 -0500

    Modificacion nombre

--- movie/views.py
+++ movie/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-	return HttpResponse('<h1>Welcome to Home Page</h1>')
+	return HttpResponse('<h1>Welcome, this page was made by Esneider Zapata Arias</h1>')
 
 # Create your views here.
