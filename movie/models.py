efbb94b091162d7440cd8a4ba1f3b699dc693b2f
Eder <ezapataa1@eafit.edu.co>
2022-04-07 10:49:43 -0500

    Actualización Allowed_Hosts

--- movie/models.py
+++ movie/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Movie(models.Model):
 	title = models.CharField(max_length=100)
 	description = models.CharField(max_length=250)
+	code = models.IntegerField()
 	image = models.ImageField(upload_to='movie/images/')
 	url = models.URLField(blank=True)
 # Create your models here.
--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_#t&&8fa&6*0ijcj8egls6kyvi1of^7_wg7u01p1sct+m$dlg^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.135.150.195']
 
 
 # Application definition
